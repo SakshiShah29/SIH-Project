9ebab626482817446fc038d96797d2f4ce54eb20
valorant-dhruv <78591597+valorant-dhruv@users.noreply.github.com>
2022-08-16 10:44:41 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import clients from "./client";
+// import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <QueryClientProvider client={clients}>
+      <ReactQueryDevtools />
+      {/* <MoralisProvider
+        serverUrl="https://utenli6iwpvw.usemoralis.com:2053/server"
+        appId="MWimdQYCKxWopxklrWuY2L2TG0QKAOATDhuExk4X"
+      > */}
+      <App />
+      {/* </MoralisProvider> */}
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );

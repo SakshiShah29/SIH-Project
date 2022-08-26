2fe08132d4fc8f66e7f1c116d06aa2758be734c3
hetparekh16 <hetparekh16@gmail.com>
2022-08-26 07:40:37 +0530

    Color change

--- client/src/components/Profilepage/Notification.js
+++ client/src/components/Profilepage/Notification.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import bell from '../../images/bell.png'
+import './Notification.css'
 const Notification = () => {
     const showNotificationDropDown = () => {
         document.querySelector('.notification-group').classList.toggle('d-none')
@@ -7,7 +8,7 @@ const Notification = () => {
     return (
         <>
             <div class="list-group notification-group d-none position-absolute">
-                <a href="#" class="list-group-item list-group-item-action">
+                <a href="#" class="list-group-item list-group-item-action list-group-item-notification">
                     <div class="d-flex w-100 justify-content-between">
                         <h5 class="mb-1">Congrats!! Your project is approved</h5>
                         {/* <small class="text-muted">3 days ago</small> */}
@@ -15,33 +16,33 @@ const Notification = () => {
                     {/* <p class="mb-1">Some placeholder content in a paragraph.</p>
                     <small class="text-muted">And some muted small print.</small> */}
                 </a>
-                <a href="#" class="list-group-item list-group-item-action">
+                <a href="#" class="list-group-item list-group-item-action list-group-item-notification">
                     <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1 c-black" style={{ color: 'black' }}>Heyy!! Sakshi Requested for Collaboration</h5>
+                        <h5 class="mb-1 c-black">Heyy!! Sakshi Requested for Collaboration</h5>
                         {/* <small class="text-muted">5 days ago</small> */}
                     </div>
                     <small class="mb-1">I saw your project, and I have some suggestion to imporve it</small>
                     {/* <small class="text-muted">And some muted small print.</small> */}
                 </a>
-                <a href="#" class="list-group-item list-group-item-action">
+                <a href="#" class="list-group-item list-group-item-action list-group-item-notification">
                     <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1 c-black" style={{ color: 'black' }}>Heyy!! Dhruv Requested for Collaboration</h5>
+                        <h5 class="mb-1 c-black">Heyy!! Dhruv Requested for Collaboration</h5>
                         {/* <small class="text-muted">5 days ago</small> */}
                     </div>
                     <small class="mb-1">I saw your project, and I have some suggestion to imporve it</small>
                     {/* <small class="text-muted">And some muted small print.</small> */}
                 </a>
-                <a href="#" class="list-group-item list-group-item-action">
+                <a href="#" class="list-group-item list-group-item-action list-group-item-notification">
                     <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1 c-black" style={{ color: 'black' }}>Heyy!! Stuti Requested for Collaboration</h5>
+                        <h5 class="mb-1 c-black">Heyy!! Stuti Requested for Collaboration</h5>
                         {/* <small class="text-muted">5 days ago</small> */}
                     </div>
                     <small class="mb-1">I saw your project, and I have some suggestion to imporve it</small>
                     {/* <small class="text-muted">And some muted small print.</small> */}
                 </a>
-                <a href="#" class="list-group-item list-group-item-action">
+                <a href="#" class="list-group-item list-group-item-action list-group-item-notification">
                     <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1 c-black" style={{ color: 'black' }}>Heyy!! Prarthana Requested for Collaboration</h5>
+                        <h5 class="mb-1 c-black">Heyy!! Prarthana Requested for Collaboration</h5>
                         {/* <small class="text-muted">5 days ago</small> */}
                     </div>
                     <small class="mb-1">I saw your project, and I have some suggestion to imporve it</small>
